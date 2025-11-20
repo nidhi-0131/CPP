ec895cbdefdd8a033d9c2306504dd0e03447b652
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2025-11-20 15:11:18 +0530

    leap year

---
+++ LeapYearCheck.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int year;
+	cout<<"Enter the year: ";
+	cin>>year;
+	if (year%400==0)
+	{
+		cout<<"The year "<<year<<" is a leap year.";
+	}
+	else if (year%100==0)
+	{
+		cout<<"The year "<<year<< " is not a leap year.";
+	}
+	else if (year%4==0)
+	{
+		cout<<"The year "<<year<<" is a leap year.";
+	}
+	else
+	{
+		cout<<"The year "<<year<< " is not a leap year.";
+	}
+}
