cb911af8da8746e6d8e933890e67bd5a4f115d04
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2025-11-24 16:04:39 +0530

    function

---
+++ function.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+void displayNum(int n1, float n2) 
+{
+    cout << "The int number is " << n1 << endl;
+    cout << "The float number is " << n2 << endl;
+}
+int main()
+{
+    int num1 = 5;
+    float num2 = 5.5;
+    displayNum(num1, num2);
+    return 0;
+}
