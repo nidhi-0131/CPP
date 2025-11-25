9078278d01b909a38c8af77ee1b6f39a6ba1f41c
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2025-11-25 20:15:02 +0530

    oe

---
+++ practice/oe.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+    if (num % 2 == 0)
+        cout << num << " is an even number.";
+    else
+        cout << num << " is an odd number.";
+    return 0;
+}
