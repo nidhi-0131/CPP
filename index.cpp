b174625cc3313bbb5d2e40933f7ef70995c370a6
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2025-12-03 12:16:55 +0530

    code collection

--- index.cpp
+++ index.cpp
@@ -618,3 +618,21 @@ int main ()
     cout << "After swapping: " << a << ", " << b << endl;
     return 0;
 }
+
+//03-12-2025: 36: factorial//
+#include <bits/stdc++.h>
+using namespace std;
+int fact(int num)
+{
+    if (num==0)
+        return 1;
+    else
+        return num*fact(num-1);
+}
+int main ()
+{
+    int n;
+    cin >> n;
+    cout << "The factorial of " << n << " is: " << fact(n) << endl;
+    return 0;
+}
