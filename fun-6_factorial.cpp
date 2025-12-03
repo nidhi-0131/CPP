0b1aebd4828814755a7f87d9fc7cae8aca73e9d5
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2025-12-03 12:16:43 +0530

    factorial

---
+++ fun-6_factorial.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fact(int num)
+{
+    if (num==0)
+        return 1;
+    else
+        return num*fact(num-1);
+}
+int main ()
+{
+    int n;
+    cin >> n;
+    cout << "The factorial of " << n << " is: " << fact(n) << endl;
+    return 0;
+}
